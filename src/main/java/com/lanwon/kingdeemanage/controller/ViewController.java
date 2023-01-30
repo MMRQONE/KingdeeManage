ae756783a73154328822d5f4e0273bf29f5fa2d5
林培满 <740641642@qq.com>
2023-01-30 16:49:01 +0800

    测试

--- src/main/java/com/lanwon/kingdeemanage/controller/ViewController.java
+++ src/main/java/com/lanwon/kingdeemanage/controller/ViewController.java
@@ -24,7 +24,6 @@ public class ViewController {
     @PostMapping({"View"})
     public Object View(@RequestBody String paramStr) throws Exception {
         JSONObject paramJson = JSON.parseObject(paramStr);
-
         String formid = (String)paramJson.get("fromid");
         String data = paramJson.get("data").toString();
         Object resultStr = this.kingeeService.view(formid, data);
